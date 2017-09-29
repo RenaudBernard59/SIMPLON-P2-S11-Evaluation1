a111c5d0f7b2c4b6b8ec06bf02fa3c4d034290a3
RenaudBernard59 <renaud.bernard@p59production.com>
2017-09-29 12:19:08 +0200

    Initializing with initializr-verekia-4.0 (Bootstrap3 + Boilerplate) modified and ParsleyJs FORM verification plugin + mvc pre processors directories

---
+++ controller/index.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once('../models/data.php');
+
+$content = getContent();
+
+
+
+
+
+
+include('../vue/indexVue.php');
---
+++ models/data.php
@@ -0,0 +1,12 @@
+<?php
+
+function getContent() {
+	return "Lala";
+};
+
+
+
+
+
+
+
